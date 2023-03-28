fc78bd70925143b4795f9f10c46d0178bc9f03ed
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-03-28 08:56:35 +0000

    [Finish]JavaScriptを学ぼう１３章

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import "script.js"
+import Swiper from 'swiper/swiper-bundle.js';
 
 Rails.start()
 Turbolinks.start()
